5b1943d947d4068d85e5b8ce474f077720d43305
Oriol Lopez <81315587+uriloop@users.noreply.github.com>
2022-05-05 01:42:36 +0200

    enemigo rpc

--- enemigo1.gd
+++ enemigo1.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
-
+puppet var puppet_player_seeking = Vector2(0, 0) 
+puppet var puppet_velocity = Vector2()
+puppet var puppet_facing = 0
 
 var speed = 200
 
@@ -10,16 +12,29 @@ var player = null
 
 func _physics_process(delta):
 	if (player):
-		var direction = (player.position - position).normalized()
-		move_and_slide(direction * speed)
+		var direction = (player.position - self.position).normalized()
+		velocity = move_and_slide(direction * speed)
 		facing = look_at(player.position)
+	else:
+		# Akí un movimiento random
+		pass
+	
+	#rpc("actualizar_enemigo")
+	
+	#no tira
+sync func actualizar_enemigo():
+	global_position=velocity
+	global_rotation=facing
+	puppet_player_seeking = player
 
 
-
-sync func _on_PlayerDetectionZone_body_entered(body):
+func _on_PlayerDetectionZone_body_entered(body):
 	if (body.is_in_group("Player") and player == null):
-		player = body
-		
+		player=body
+	#	
+	elif (body.is_in_group("Player") and player.distance_to(self)>=body.distance_to(self)):
+		player=body
+
 
 
 func _on_hurtBox_area_entered(area):
