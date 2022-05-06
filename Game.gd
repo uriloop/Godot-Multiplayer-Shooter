86340f46a7ba6a64fffbf56d6f1bc2a39614a33a
Oriol Lopez <81315587+uriloop@users.noreply.github.com>
2022-05-06 17:46:01 +0200

    daw

--- Game.gd
+++ Game.gd
@@ -38,7 +38,7 @@ func _player_disconnected(id) -> void:
 var rng = RandomNumberGenerator.new()
 
 sync func instance_enemy1(id):
-	var enemy1_instance = enemy_scene.instance_node_at_location(enemy_scene,Persistent_nodes, random_spawn_enemy_position())
+	var enemy1_instance = Global.instance_node_at_location(enemy_scene,Persistent_nodes, random_spawn_enemy_position())
 	enemy1_instance.name = "Enemy1" + str(Network.networked_object_name_index)
 	enemy1_instance.set_network_master(1)
 #	enemy1_instance.global_position=random_spawn_enemy_position();
